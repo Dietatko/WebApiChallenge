3fbbce6d6429b16ed5b8dd7aaf93c4432688e232
Dietatko <burgera@gmail.com>
2018-12-01 17:43:05 +0000

    Initial project structure created

---
+++ Source/Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckoutChallenge.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
